2ca407fc72b02080bc99c3cd82e33e4b3f39bc8a
Sarah Smith <sarahsmithvii@gmail.com>
2023-03-01 20:39:12 +0000

    initial commit

---
+++ week_01_day_03_python_functions_practice.py
@@ -0,0 +1,54 @@
+# Where to write functions
+
+def return_10():
+    return 10
+
+def add(num1, num2):
+    add_result = num1 + num2
+    return add_result
+
+def subtract(num1, num2):
+    subtract_result = num1 - num2
+    return subtract_result
+
+def multiply(num1, num2):
+    multiply_result = num1 * num2
+    return multiply_result
+
+def divide(num1, num2):
+    divide_result = num1 / num2
+    return divide_result
+
+def length_of_string(string):
+    length = len(string)
+    return length
+
+def join_string(part_1, part_2):
+    return part_1 + part_2
+
+def add_string_as_number(num1, num2):
+    return int(num1) + int(num2)
+
+def number_to_full_month_name(month_number):
+    calendar = {1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July", 8: "August", 9: "September", 10: "October", 11: "November", 12: "December"} 
+    return calendar[month_number]
+
+def number_to_short_month_name(month_number):
+    calendar = {1: "Jan", 2: "Feb", 3: "Mar", 4: "Apr", 5: "May", 6: "Jun", 7: "Jul", 8: "Aug", 9: "Sept", 10: "Oct", 11: "Nov", 12: "Dec"}
+    return calendar[month_number]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
